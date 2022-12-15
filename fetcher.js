283bd8680df1533ddf6f1fe32f60a41b539cf528
DASitby <davidsooley551@gmail.com>
2022-12-15 22:52:11 +0000

    add explanation comments

--- fetcher.js
+++ fetcher.js
@@ -5,14 +5,17 @@ const filePath = args[1];
 //Imports
 const request = require('request');
 const fs = require('fs');
-
+//Request URL
 request(URL, (error, response, body) => {
+  //Log Errors and/or Status Code
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //write the body of the webpage to the file path
   fs.writeFile(`${filePath}`,body, err => {
     if (err) {
       console.error(err);
     }
   });
+  //log success and the file size
   console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`); // Print the HTML for the Google homepage.
 });
